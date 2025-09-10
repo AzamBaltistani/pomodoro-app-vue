6a7ac2a081891791d7a6482592fb9b0d8dbcb81c
AzamBaltistani <sikanderazam276@gmail.com>
2025-09-10 10:49:21 +0500

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/pomodoro-app-vue'
+  base: '/pomodoro-app-vue/'
 })
